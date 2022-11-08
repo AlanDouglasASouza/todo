f4c86f26ed498b4386109b602882390eb4caf896
Alan <alan.douglas.ads@gmail.com>
2022-11-07 21:35:34 -0300

    chore: updates to a cleaner code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,33 @@
+fn main() {
+    println!("Olá! 😃");
+
+    loop {
+        println!("");
+        println!("Você gostaria de adicionar um novo TODO? 🤔 (Digite: 's' para SIM ou qualquer outra tecla para NÃO)");
+
+        if input() == "s" {
+            create_todo();
+        } else {
+            println!();
+            println!("Todo list finalizado! 🤠");
+            break;
+        }        
+    }
+}
+
+fn create_todo() {
+    println!();
+
+    println!("Qual TODO deseja criar?");
+    let todo = input();
+
+    println!();
+
+    println!("✅: {todo}");
+}
+
 fn input() -> String {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
     buf.trim().to_string()
  }
-
-fn main() {
-     
-    let mut response: String = "s".to_string();
-
-    println!("Olá! 😃");
-
-    while response == "s" {
-        println!("");
-        println!("Você gostaria de adicionar um novo TODO? 🤔 (Digite: 's' para SIM ou qualquer outra tecla para NÃO)");
-
-        response = input();
-
-        if response == "s" {
-            create_todo();
-        } else {
-            println!("");
-            println!("Todo list finalizado! 🤠");
-        }  
-    }
-}
-
-fn create_todo() {
-    println!("");
-
-    println!("Qual TODO deseja criar?");
-    let todo = input();
-
-    println!("");
-
-    println!("✅: {}", todo);
-}
