b78745d43a27c005e1438dc165d54c9e4667292d
Alan <alan.douglas.ads@gmail.com>
2022-11-15 20:53:46 -0300

    chore: added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::io::{Stdin, Stdout, Write};
+use std::io;
 
 fn main() {
     println!("Olá! 😃");
 
     loop {
        let mut ask_todo = Terminal::new();
-       let todo = ask_todo.ask_for_new_todo();
-
-       ask_todo.show_todo(&todo);
+       let todo = ask_todo.ask_for_new_todo();       
+       
+       if let Err(_) = ask_todo.show_todo(&todo) {
+        Terminal::show_error(&mut ask_todo, Err(TerminalError::WriteErr));
+       }
     }
 }
 
@@ -32,7 +35,10 @@ impl Terminal {
 
     fn should_ask_for_todo(&mut self) -> bool {
         let mut buf = String::new();
-        self.stdin.read_line(&mut buf).unwrap();
+
+        if let Err(_) = self.stdin.read_line(&mut buf) {
+            self.show_error(Err(TerminalError::StdoutErr));
+        }
 
         buf.trim() == "s"
     }
@@ -49,13 +55,41 @@ impl Terminal {
         println!("\nQual TODO deseja criar?");
 
         let mut new_todo = String::new();
-        self.stdin.read_line(&mut new_todo).unwrap();
+
+        if let Err(_) = self.stdin.read_line(&mut new_todo) {
+            self.show_error(Err(TerminalError::StdoutErr));
+        }
+
         let todo_message = new_todo.trim().to_string();
 
         Todo { message: todo_message }
     }
 
-    fn show_todo(&mut self, todo: &Todo) {        
-        writeln!(self.stdout, "\n✅: {}", todo.message).unwrap();
+    fn show_todo(&mut self, todo: &Todo) -> Result<(), io::Error> { 
+        writeln!(self.stdout, "\n✅: {}", todo.message)?;
+        Ok(())
+    }
+
+    fn show_error(&mut self, data: Result<(), TerminalError>) {
+
+        if let Err(error) = data {
+            println!("{}", TerminalError::message_err(error));
+        }
+       
+    }
+}
+
+enum TerminalError {    
+    WriteErr,
+    StdoutErr,
+}
+
+impl TerminalError {
+
+    fn message_err(msg: TerminalError) -> String {
+        match msg {            
+            Self::WriteErr => "Houve um erro ao tentar exibir mensagem".to_string(),
+            Self::StdoutErr => "Houve um erro na entrada de dados".to_string(),      
+        }
     }
 }
