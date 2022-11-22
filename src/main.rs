529c990771d47994a43988420d2435d33347a40d
Alan <alan.douglas.ads@gmail.com>
2022-11-22 15:38:20 -0300

    refac: refactor error handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 use std::io::{Stdin, Stdout, Write, Error};
-use std::io;
 
 fn main() {
     println!("Olá! 😃");
 
     loop {
-       let mut ask_todo = Terminal::new();
-       let todo = ask_todo.ask_for_new_todo();
+        let mut ask_todo = Terminal::new();
+        println!("\nVocê gostaria de adicionar um novo TODO? 🤔 (Digite: 's' para SIM ou 'n' para NÃO)");
+             
+        match ask_todo.should_ask_for_todo() {
+            Ok(UserResponse::No) => {
+                println!("\n😁 Ok!! Todo list finalizado! 🤠\n");
+                break;                 
+            },
+            Ok(UserResponse::Other) => {
+                println!("\n🙁 Desculpe esse comando não é válido para esse processo...");
+                ()
+            },
+            Ok(UserResponse::Yes) => {
+                let todo = ask_todo.ask_for_new_todo();
 
-       match todo {
-           Ok(new_todo) => {            
-                if let Err(error) = ask_todo.show_todo(&new_todo) {
-                    ask_todo.show_error(TerminalError::WriteErr(error))
-                }        
-           },
-           Err(error) => ask_todo.show_error(error)
-       }       
+                match todo {
+                    Ok(new_todo) => {            
+                            if let Err(error) = ask_todo.show_todo(&new_todo) {
+                                ask_todo.show_error(error)
+                            }        
+                    },
+                    Err(error) => ask_todo.show_error(error)
+                }
+            },
+            Err(err) => ask_todo.show_error(err),            
+        }
     }
 }
 
@@ -38,49 +52,40 @@ impl Terminal {
         }
     }
 
-    fn should_ask_for_todo(&mut self) -> Result<bool, TerminalError> {
+    fn should_ask_for_todo(&mut self) -> Result<UserResponse, TerminalError> {
         let mut buf = String::new();
-
         self.stdin.read_line(&mut buf).map_err(TerminalError::StdoutErr)?;
 
-        Ok(buf.trim() == "s")
+        match buf.trim() {
+            "s" => Ok(UserResponse::Yes),
+            "n" => Ok(UserResponse::No),
+            _ => Ok(UserResponse::Other),
+        }
     }
 
     fn ask_for_new_todo(&mut self) -> Result<Todo, TerminalError> {
 
-        println!("\nVocê gostaria de adicionar um novo TODO? 🤔 (Digite: 's' para SIM ou qualquer outra tecla para NÃO)");
-                
-        if let Ok(false) = self.should_ask_for_todo() {
-            println!("\nTodo list finalizado! 🤠");
-            std::process::exit(0);            
-        } 
-
         println!("\nQual TODO deseja criar?");
-
         let mut new_todo = String::new();
-
         self.stdin.read_line(&mut new_todo).map_err(TerminalError::StdoutErr)?;
 
         let todo_message = new_todo.trim().to_string();
 
-        Ok(Todo { message: todo_message })
-       
+        Ok(Todo { message: todo_message })       
     }
 
-    fn show_todo(&mut self, todo: &Todo) -> Result<(), io::Error> { 
-        writeln!(self.stdout, "\n✅: {}", todo.message)?;
+    fn show_todo(&mut self, todo: &Todo) -> Result<(), TerminalError> { 
+        writeln!(self.stdout, "\n✅: {}", todo.message).map_err(TerminalError::WriteErr)?;
         Ok(())
     }
 
-    fn show_error(&mut self, error: TerminalError) { 
-        
-        eprintln!("{}", error.message_err());
-        
+    fn show_error(&mut self, error: TerminalError) {        
+        eprintln!("{}", error.message_err());        
     }
 }
 
 #[derive(Debug)]
-enum TerminalError {    
+enum TerminalError {
     WriteErr(Error),
     StdoutErr(Error),
 }
@@ -93,4 +98,10 @@ impl TerminalError {
             Self::StdoutErr(err) => format!("Houve um erro na entrada de dados {}", err)
         }
     }
+}
+
+enum UserResponse {
+    Yes,
+    No,
+    Other
 }
